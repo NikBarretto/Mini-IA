1315cb7fe80c9d73739f4b2b4c5b1cabfec4dc0c
NikBarretto <98937075+NikBarretto@users.noreply.github.com>
2022-05-16 11:19:58 +0530

    Add files via upload

---
+++ Product/menu.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <center>
+        <a class="a" href="index.php">Home</a>&nbsp
+        <a class="a" href="about.php">About Us</a>&nbsp
+        <a class="a" href="login">Login</a>&nbsp
+        <a class="a" href="gallery.php">Gallery</a>&nbsp
+        <a class="a" href="contact.php">Contact Us</a>&nbsp
+        <a class="a" href="register.php">Register</a>&nbsp
+        <br> <br>
+        <h1> Mini IA </h1>
+    </center>
+</body>
+</html>
